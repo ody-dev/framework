f607cef1ec7bfead026f9eabc75bb1efb74ba531
Ilyas <deckersilyas@gmail.com>
2025-03-18 07:45:31 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -34,7 +34,8 @@ return [
 //            \Ody\Foundation\Middleware\ErrorHandlerMiddleware::class,
             \Ody\Foundation\Middleware\CorsMiddleware::class,
             \Ody\Foundation\Middleware\JsonBodyParserMiddleware::class,
-            \Ody\Foundation\Middleware\LoggingMiddleware::class
+            \Ody\Foundation\Middleware\LoggingMiddleware::class,
+            \App\Middleware\RequestLoggerMiddleware::class, 
         ],
         'named' => [
             'api' => [
